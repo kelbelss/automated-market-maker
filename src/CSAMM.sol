de815ca85d0560f6859d3430dc35b2c57c38e868
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-03-01 12:01:55 +0200

    Set Up Contracts and ERC20

---
+++ src/CSAMM.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract CSAMM {
+
+    IERC20 public immutable token0;
+    IERC20 public immutable token1;
+    
+    function swap() external {}
+
+    function addLiquidity() external {}
+
+    function removeLiquidity() external {}
+    
+
+}
